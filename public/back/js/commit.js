754a36eb07eb5b02de1c95ec59b76051007d3ca2
zzc <774143745@qq.com>
2017-10-29 20:26:25 +0800

    公共样式和他们的js

--- public/back/js/commit.js
+++ public/back/js/commit.js
@@ -1,3 +1,17 @@
+;(function(){
+    //判断用户是否登录
+    if(location.href.indexOf("login.html") === -1) {
+        $.ajax({
+            type:"get",
+            url:'/employee/checkRootLogin',
+            success: function (data) {
+                if(data.error) {
+                    location.href = "login.html";
+                }
+            }
+        })
+    }
+})();
 ;(function () {
     $(document).ajaxStart(function () {
         NProgress.start();
@@ -7,4 +21,26 @@
             NProgress.done();
         }, 500);
     });
+})();
+;(function () {
+    $('.lt_cate').on("click",function(){
+        $(".cate_son").slideToggle();
+    });
+
+    $(".top_left_icon").on("click", function () {
+        $(".aside").toggleClass("active");
+        $("section").toggleClass("active");
+    })
+
+    $("#quit").on('click', function () {
+        $.ajax({
+            type:'get',
+            url:'/employee/employeeLogout',
+            success: function (data) {
+                if(data.success) {
+                    location.href = "login.html";
+                }
+            }
+        })
+    })
 })();
