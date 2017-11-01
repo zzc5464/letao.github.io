7495cec5e462faf58e86efdc76957273fdb360a1
zzc <774143745@qq.com>
2017-11-01 19:17:29 +0800

    index静态

---
+++ public/front/js/commont.js
@@ -0,0 +1,7 @@
+/**
+ * Created by zzc on 2017/11/1.
+ */
+var sc = mui('.mui-scroll-wrapper').scroll({
+    indicators: false,
+    deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+});
---
+++ public/front/js/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by zzc on 2017/11/1.
+ */
+//轮播图效果
+mui('.mui-slider').slider({
+    interval:1000//自动轮播周期，若为0则不自动播放，默认为0；
+});
