9488efc6ce597db4dd180a59325c751b7c10224d
zzc <774143745@qq.com>
2017-11-01 22:39:22 +0800

    封装工具类

--- public/front/js/commont.js
+++ public/front/js/commont.js
@@ -5,3 +5,26 @@ var sc = mui('.mui-scroll-wrapper').scroll({
     indicators: false,
     deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
 });
+
+//工具类
+class Tool {
+    constructor(){}
+    getParamObj(){
+        var search = location.search;
+        search = search.slice(1);
+        var searchArr = search.split("&");
+        var searchObj = {};
+        for (var i = 0; i < searchArr.length; i++) {
+            var key = searchArr[i].split("=")[0];
+            var value = decodeURI(searchArr[i].split("=")[1]);
+            //把属性名和值存储到对象中
+            searchObj[key] = value;
+        }
+        return searchObj;
+    }
+    getParam(key){
+        return this.getParamObj()[key];
+    }
+}
+var t = new Tool();
+
